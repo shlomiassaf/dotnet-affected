addf7c818dfdefc81ab0902ef973fb9958fcdb4e
Shlomi Assaf <shlomiasaf@gmail.com>
2022-09-10 21:18:07 +0300

    feature: support project resolution in import statements

--- src/DotnetAffected.Core/GitChangesProvider.cs
+++ src/DotnetAffected.Core/GitChangesProvider.cs
@@ -1,9 +1,15 @@
 ﻿using DotnetAffected.Abstractions;
 using LibGit2Sharp;
+using Microsoft.Build.Construction;
+using Microsoft.Build.Definition;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Evaluation.Context;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Xml;
 
 namespace DotnetAffected.Core
 {
@@ -12,6 +18,22 @@ namespace DotnetAffected.Core
     /// </summary>
     public class GitChangesProvider : IChangesProvider
     {
+        private static readonly Lazy<bool> ResolveMsBuildFileSystemSupported = new Lazy<bool>(() =>
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(typeof(Project).Assembly.Location);
+            if (versionInfo.FileMajorPart > 16)
+                return true;
+            if (versionInfo.FileMajorPart < 16)
+                return false;
+            return versionInfo.FileMinorPart >= 10;
+        });
+
+        /// <summary>
+        /// When true, the build system supports virtual filesystem which means nested Directory.Packages.props files
+        /// are supported in central package management.
+        /// </summary>
+        public static bool MsBuildFileSystemSupported => ResolveMsBuildFileSystemSupported.Value;
+
         /// <inheritdoc />
         public IEnumerable<string> GetChangedFiles(string directory, string from, string to)
         {
@@ -22,46 +44,109 @@ namespace DotnetAffected.Core
             return TreeChangesToPaths(changes, directory);
         }
 
+        public Project? LoadDirectoryPackagePropsProject(string directory, string pathToFile, string? commitRef, bool fallbackToHead)
+        {
+            var project = LoadProject(directory, pathToFile, commitRef, fallbackToHead);
+            if (project is null && MsBuildFileSystemSupported)
+            {
+                var fi = new FileInfo(pathToFile);
+                var parent = fi.Directory?.Parent?.FullName;
+                if (parent is not null && parent.Length >= directory.Length)
+                    return LoadDirectoryPackagePropsProject(directory, Path.Combine(parent, "Directory.Packages.props"), commitRef, fallbackToHead);
+            }
+
+            return project;
+        }
+
         /// <inheritdoc />
-        public (string? FromText, string? ToText) GetTextFileContents(
-            string directory,
-            string pathToFile,
-            string from,
-            string to)
+        public Project? LoadProject(string directory, string pathToFile, string? commitRef, bool fallbackToHead)
         {
+            return MsBuildFileSystemSupported
+                ? LoadProjectCore(directory, pathToFile, commitRef, fallbackToHead)
+                : LoadProjectLegacy(directory, pathToFile, commitRef, fallbackToHead);
+        }
+
+        private Project? LoadProjectCore(string directory, string pathToFile, string? commitRef, bool fallbackToHead)
+        {
+            Commit? commit;
+            
             using var repository = new Repository(directory);
+            
+            if (string.IsNullOrWhiteSpace(commitRef))
+                commit = fallbackToHead ? repository.Head.Tip : null;
+            else
+                commit = GetCommitOrThrow(repository, commitRef);
 
-            var (fromCommit, toCommit) = ParseRevisionRanges(repository, from, to);
+            var fs = new MsBuildGitFileSystem(repository, commit);
 
-            pathToFile = Path.GetRelativePath(directory, pathToFile);
+            if (fs.FileExists(pathToFile))
+            {
+                var projectCollection = new ProjectCollection();
+                
+                // Loading using a file path will not work since creating/opening the root project is done directly, no virtual FS there.
+                // We must use a reader so we control where the content comes.
+                // Later, when we attach it to a Project, imports will be loaded via the git file system...
+                using var reader = new XmlTextReader(fs.GetFileStream(pathToFile, FileMode.Open, FileAccess.Read, FileShare.None));
+                var projectRootElement = ProjectRootElement.Create(reader, projectCollection);
 
-            // Read file from commit or working directory
-            var fromText = fromCommit is null
-                ? ReadTextFileFromWorkingDirectory(directory, pathToFile)
-                : ReadTextFileFromCommit(pathToFile, fromCommit);
+                // Creating from an XML reader does not have a file system context, i.e. it does not know the root path.
+                // It will use the process root path.
+                // We need to set the exact path so dynamic imports are resolved relative to the original path.
+                // Not that this one must be the absolute path, not relative!
+                projectRootElement.FullPath = pathToFile;
 
-            var toText = ReadTextFileFromCommit(pathToFile, toCommit);
+                return Project.FromProjectRootElement(projectRootElement, new ProjectOptions
+                {
+                    LoadSettings = ProjectLoadSettings.Default,
+                    ProjectCollection = projectCollection,
+                    EvaluationContext = EvaluationContext.Create(EvaluationContext.SharingPolicy.Shared, fs),
+                });
+            }
 
-            return (fromText, toText);
+            return null;
         }
 
-        private static string? ReadTextFileFromWorkingDirectory(string directory, string pathToFile)
+        private Project? LoadProjectLegacy(string directory, string pathToFile, string? commitRef, bool fallbackToHead)
         {
-            var path = Path.Combine(directory, pathToFile);
-            if (!File.Exists(path)) return null;
+            Commit? commit;
+            
+            using var repository = new Repository(directory);
+            
+            if (string.IsNullOrWhiteSpace(commitRef))
+                commit = fallbackToHead ? repository.Head.Tip : null;
+            else
+                commit = GetCommitOrThrow(repository, commitRef);
 
-            return File.ReadAllText(path);
-        }
+            Stream GenerateStreamFromString(string s)
+            {
+                var stream = new MemoryStream();
+                var writer = new StreamWriter(stream);
+                writer.Write(s);
+                writer.Flush();
+                stream.Position = 0;
+                return stream;
+            }
 
-        private static string? ReadTextFileFromCommit(string pathToFile, Commit commit)
-        {
-            var treeEntry = commit[pathToFile];
-            if (treeEntry == null) return null;
+            if (commit is null)
+            {
+                var path = Path.Combine(directory, pathToFile);
+                if (!File.Exists(path)) return null;
 
-            var blob = (Blob)treeEntry.Target;
+                using var reader = new XmlTextReader(GenerateStreamFromString(File.ReadAllText(path)));
+                return new Project(reader);
+            }
+            else
+            {
+                var path = Path.GetRelativePath(directory, pathToFile);
+                var treeEntry = commit[path];
+                if (treeEntry == null) return null;
 
-            using var content = new StreamReader(blob.GetContentStream(), Encoding.UTF8);
-            return content.ReadToEnd();
+                var blob = (Blob)treeEntry.Target;
+
+                using var content = new StreamReader(blob.GetContentStream(), Encoding.UTF8);
+                using var reader = new XmlTextReader(GenerateStreamFromString(content.ReadToEnd()));
+                return new Project(reader);
+            }
         }
 
         private static (Commit? From, Commit To) ParseRevisionRanges(
